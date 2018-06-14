2977889f263b2d0c6fb05889340fa159f4c6a99e
Jeremy S. Perkins <jeremyshane@gmail.com>
2018-06-14 13:32:14 -0400

    Changes up to this point (this is a useless message).

--- Fermi_Rain.py
+++ Fermi_Rain.py
@@ -13,9 +13,16 @@ from astropy.io import fits
 import matplotlib as mpl
 from astropy.time import Time
 
+# 2017-01-01 00:00:00.000
+metstart = 504921604.000
+# 2017-12-31 23:59:59.999
+metstop = 536457604.999
+
 # Open all of the needed files and setup some things
-hdu = fits.open('prob_ft1_00.fits')
-mask = hdu[1].data['3FGL J0509.4+0541'] > 0.85
+hdu = fits.open('events_gti_srcProb.fits')
+mask = ((hdu[1].data['3FGL J0509.4+0541'] > 0.85))
+#  & (hdu[1].data['TIME'] > metstart)
+#  & (hdu[1].data['TIME'] < metstop))
 RA = (hdu[1].data['RA'][mask]-np.average(hdu[1].data['RA'][mask]) + 1)/2.
 DEC = (hdu[1].data['DEC'][mask]-np.average(hdu[1].data['DEC'][mask]) + 1)/2.
 TIMEMET = hdu[1].data['TIME'][mask]
@@ -28,7 +35,7 @@ MJDREFF = hdu[0].header['MJDREFF']
 TIMEMJD = hdu[1].data['TIME'] / 86400. + MJDREFI + MJDREFF
 
 # This is the width (in seconds) of each frame
-WIDTH = 60. * 10.
+WIDTH = 86400.
 FRAMES = np.int(TIME[-1] / WIDTH)
 
 
@@ -41,11 +48,15 @@ cmap = mpl.cm.ScalarMappable(norm=cm_norm, cmap=cm)
 
 # Create new Figure and an Axes which fills it.
 my_dpi = 72
-fig = plt.figure(figsize=(800/my_dpi, 800/my_dpi),
+fig = plt.figure(figsize=(1600/my_dpi, 900/my_dpi),
                  dpi=my_dpi, facecolor='black')
 ax = fig.add_axes([0, 0, 1, 1], frameon=False)
-ax.set_xlim(0, 1), ax.set_xticks([])
+ax.set_xlim(-0.38, 1.38), ax.set_xticks([])
 ax.set_ylim(0, 1), ax.set_yticks([])
+
+# Use these for sidebars if you want
+# ax1 = fig.add_axes([0, 0, 0.2, 1], frameon=False)
+# ax2 = fig.add_axes([0.8, 0, 0.2, 1], frameon=False)
 
 # Create rain data
 n_drops = len(TIME)
@@ -54,8 +65,8 @@ rain_drops = np.zeros(n_drops, dtype=[('position', float, 2),
                                       ('growth',   float, 1),
                                       ('color',    float, 4)])
 
-rain_drops['position'] = zip(RA, DEC)
-rain_drops['growth'] = ENERGY/10.
+rain_drops['position'] = list(zip(RA, DEC))
+rain_drops['growth'] = ENERGY/2.
 rain_drops['color'] = cmap.to_rgba(np.log10(ENERGY))
 rain_drops['color'][:, 3] = 0
 
@@ -63,16 +74,15 @@ rain_drops['color'][:, 3] = 0
 # Construct the scatter which we will update during animation
 # as the raindrops develop.
 scat = ax.scatter(rain_drops['position'][:, 0], rain_drops['position'][:, 1],
-                  s=rain_drops['size'], lw=1.0, edgecolors=rain_drops['color'],
+                  s=rain_drops['size'], lw=2.0, edgecolors=rain_drops['color'],
                   facecolors='none')
 
-
 # Construct the histogram for the counts map
-H, xedges, yedges = np.histogram2d(rain_drops['position'][:, 0],
-                                   rain_drops['position'][:, 1], bins=15)
-im = plt.imshow(H, interpolation='none', origin='low',
-                extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]],
-                cmap='bone', vmin=5, vmax=H.max()*2.0)
+# H, xedges, yedges = np.histogram2d(rain_drops['position'][:, 0],
+#                                   rain_drops['position'][:, 1], bins=15)
+# im = ax.imshow(H, interpolation='none', origin='low',
+#               extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]],
+#               cmap='bone', vmin=5, vmax=H.max()*2.0)
 
 # Uncomment this out if you want the energy text printed
 # E_txt = [ax.text(rain_drops['position'][i, 0],
@@ -80,18 +90,38 @@ im = plt.imshow(H, interpolation='none', origin='low',
 #                 color=rain_drops['color'][0])
 #         for i,Es in enumerate(E_str)]
 
+# Construct the histogram for the light curve
+# frq, edges = np.histogram(TIME, bins=100)
+# ln1, = ax1.plot(frq, edges[:-1], 'wo-', alpha=0.5, animated=False)
+# fb1 = ax1.fill_betweenx(edges[:-1], 0, frq, alpha=0.2)
+
+
+# Construct the histogram for the energy curve
+def get_max(low, high):
+    mask = (TIME > low) & (TIME < high)
+    if np.sum(mask) > 0:
+        return -1*np.max(ENERGY[mask])
+    else:
+        return 0
+
+
+# frq_E = np.array([get_max(edge, edges[i+1])
+#                  for i, edge in enumerate(edges[:-1])])
+# ln2, = ax2.plot(frq_E, edges[:-1], 'wo-', alpha=0.5, animated=False)
+# fb2 = ax2.fill_betweenx(edges[:-1], 0, frq_E, alpha=0.2)
 
 # This is the inital time step
 TIMEMJD = (TIMEMET[0])/86400. + MJDREFI + MJDREFF
 t = Time(TIMEMJD, format='mjd')
-txt_time = ax.text(0.8, 0.9, t.datetime.strftime("%d %b %Y %H:%M"))
+# txt_time = ax.text(0.8, 0.9, t.datetime.strftime("%d %b %Y %H:%M"))
+txt_time = ax.text(1.2, 0.9, t.datetime.strftime("%d %b %Y"))
 txt_time.set_color((1, 1, 1, 1))
 
 # This is the line showing the scale
-txt_scale = ax.text(0.13, 0.07, r'0.2$^\circ$')
-txt_scale.set_color((1, 1, 1, 1))
-line, = ax.plot([0.1, 0.2], [0.1, 0.1], 'k-')
-line.set_color((1, 1, 1, 1))
+# txt_scale = ax.text(-0.265, 0.07, r'0.2$^\circ$')
+# txt_scale.set_color((1, 1, 1, 1))
+# line, = ax.plot([-0.2, -0.3], [0.1, 0.1], 'k-')
+# line.set_color((1, 1, 1, 1))
 
 
 def update(frame_number):
@@ -103,7 +133,8 @@ def update(frame_number):
     TIMEMJD = (TIMEMET[0] + (frame_number + 1) * WIDTH)/86400.
     TIMEMJD += MJDREFI + MJDREFF
     t = Time(TIMEMJD, format='mjd')
-    txt_time.set_text(t.datetime.strftime("%d %b %Y %H:%M"))
+    # txt_time.set_text(t.datetime.strftime("%d %b %Y %H:%M"))
+    txt_time.set_text(t.datetime.strftime("%d %b %Y"))
 
     # Make all colors more transparent as time progresses.
     rain_drops['color'][:, 3][mask] -= 1.0/50
@@ -126,16 +157,21 @@ def update(frame_number):
     rain_drops['color'][:, 3][new] = np.clip(rain_drops['color'][:, 3][new],
                                              0, 1)
 
-    # Update the histogram with a new frame
-    hist = np.histogram2d(rain_drops['position'][mask][:, 0],
-                          rain_drops['position'][mask][:, 1])
-    im.set_data(hist[0])
+    # Update the 2d histogram with a new frame
+    # hist = np.histogram2d(rain_drops['position'][mask][:, 0],
+    #                      rain_drops['position'][mask][:, 1])
+    # im.set_data(hist[0])
 
     # Update the scatter collection, with the new colors, sizes and positions.
     scat.set_edgecolors(rain_drops['color'])
     scat.set_sizes(rain_drops['size'])
     scat.set_offsets(rain_drops['position'])
 
+    # Update the light curve with new data
+    # lcmask = edges < (frame_number + 1) * WIDTH
+    # ln1.set_data(frq[lcmask[:-1]], edges[lcmask])
+    # ln2.set_data(frq_E[lcmask[:-1]], edges[lcmask])
+    
     
 # Construct the animation, using the update function as the animation
 # director.
